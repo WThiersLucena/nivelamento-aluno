8550f58ece3ebd6c69343dc355a4be530b7b748f
Wanderson <thiers_@hotmail.com>
2021-07-08 15:23:18 -0300

    aula

---
+++ 01-exercicio.rb
@@ -0,0 +1,11 @@
+puts " quantas horas há em um ano?" 
+puts 24*365
+puts " quantos minutos há em uma década?" 
+puts 60*24* 365*10
+puts " qual é a sua idade em segundos?" 
+puts 60*60*24*365*35
+puts "quantos chocolates você pretende comer na vida?" 
+puts 35*12/8
+puts " Desafio:"
+puts "- Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?"
+puts 1232000000/365/24/60/60
